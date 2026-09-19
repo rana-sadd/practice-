6483bc71b0c14f3cbd7fb357ef79436f24290cec
rana-sadd <ranasadu.g@gmail.com>
2026-09-19 12:16:11 +0500

    its project

--- demo.cpp
+++ demo.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
 using namespace std;
-int main() {
-    int a, b;
-    cout << "Enter two numbers: ";
-    cin >> a >> b;
-    int sum = a + b;
-    cout << "Sum: " << sum << endl;
+class employee{
+    public:
+    string name;
+    int age;
+    void display(){
+        cout<<"Name: "<<name<<endl;
+        cout<<"Age: "<<age<<endl;
+    }
+};
+int main(){
+    employee emp1;
+    emp1.name = "John Doe";
+    emp1.age = 30;
+    emp1.display();
     return 0;
 }
