ca98020d28438bc7374d1230b4829a39f25a9604
rana-sadd <ranasadu.g@gmail.com>
2026-09-19 11:27:09 +0500

    add class in program

--- c++.cpp
+++ c++.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 using namespace std;
+class student{
+    public:
+    string name;
+    int age;
+    void display(){
+        cout<<"Name: "<<name<<endl;
+        cout<<"Age: "<<age<<endl;
+    }
+};
+    
 int main (){
-    cout<<"Hello, World!";
+    student s1;
+    s1.name = "Alice";
+    s1.age = 20;
+    s1.display();
     return 0;
 }
